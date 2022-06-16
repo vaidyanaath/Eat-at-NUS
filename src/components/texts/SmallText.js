520bc24e9a578c10fdffaa66d964d3dce77971b2
Vaidyanaath Suresh <svaidyanaath@gmail.com>
2022-06-16 23:05:17 +0530

    Add customer sign in screen with dummy buttons

---
+++ src/components/texts/SmallText.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+import { colors } from "../../assets/colors";
+
+export const SmallText = styled.Text`
+    font-size: 15px;
+    font-family: 'SourceSansPro-Regular';
+    color: ${ colors.secondary };
+`;
