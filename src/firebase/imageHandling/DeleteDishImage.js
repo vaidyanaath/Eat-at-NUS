c1f46971ac137e9ccc6428746304fae5b21673c6
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2022-08-02 23:29:58 +0800

    Add deleteDishImage function

---
+++ src/firebase/imageHandling/DeleteDishImage.js
@@ -0,0 +1,31 @@
+// Importing the required stuff
+import { db, storage } from '../config';
+import { ref, get, update } from 'firebase/database';
+import { ref as storageRef, deleteObject } from 'firebase/storage';
+
+const deleteDishImage = async (stallID, dishID) => {
+  let fileType = '';
+
+  await get(ref(db, 'dishes/' + dishID + '/imageURL')).then((snapshot) => {
+    const dishImageURL = snapshot.val();
+    fileType = dishImageURL.slice(dishImageURL.lastIndexOf('.'), dishImageURL.lastIndexOf('?'));
+  });
+
+  const dishImageFilePath = 'dishes/' + stallID + '/' + dishID + fileType;
+
+  await deleteObject(storageRef(storage, dishImageFilePath)).catch((error) => {
+    console.log(error);
+  });
+
+  // Updating the data in dishes
+  update(ref(db, 'dishes/' + dishID), {
+    imageURL: '',
+  });
+
+  // Updating the data in dishesMetadata
+  update(ref(db, 'dishesMetadata/' + stallID + '/' + dishID), {
+    imageURL: '',
+  });
+};
+
+export default deleteDishImage;
