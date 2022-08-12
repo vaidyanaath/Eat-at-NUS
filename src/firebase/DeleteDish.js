c4debbb522cdfb98cae64c5ffe9847dcbc4bee17
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2022-08-12 23:23:47 +0800

    Fix bugs in deleteDish function

--- src/firebase/DeleteDish.js
+++ src/firebase/DeleteDish.js
@@ -7,11 +7,17 @@ const deleteDish = async (stallID, dishID) => {
   // Delete dish image from firebase storage if it has an image
   await deleteDishImage(stallID, dishID);
 
+  // Delete dish from dishes
   const dishReference = ref(db, 'dishes/' + dishID);
   await remove(dishReference);
 
+  // Delete dish from dishesMetadata
   const dishMetadataReference = ref(db, 'dishesMetadata/' + stallID + '/' + dishID);
   await remove(dishMetadataReference);
+
+  // Delete all reviews for the dish
+  const dishReviewsReference = ref(db, 'reviews/' + dishID);
+  await remove(dishReviewsReference);
 };
 
 export default deleteDish;
