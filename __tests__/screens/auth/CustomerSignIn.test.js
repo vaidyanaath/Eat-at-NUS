4bc057e6f13c65ca8a14d1c49ef2cab56e504152
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2022-08-14 22:58:19 +0800

    Fix minor bugs in customerSignIn test

--- __tests__/screens/auth/CustomerSignIn.test.js
+++ __tests__/screens/auth/CustomerSignIn.test.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { render, fireEvent } from '@testing-library/react-native';
-import { getAuth } from 'firebase/auth';
 
 import CustomerSignIn from '../../../src/screens/auth/CustomerSignIn';
 
-jest.mock('firebase/auth', () => {
-    return {
-        getAuth: jest.fn();
-    }
-});
-
-describe('Firebase Auth Mock', () => {
-
-    it('Should return user', () => {
-        const auth = getAuth();
-        
-    });
-});
-
 describe('<CustomerSignIn />', () => {
-
-    it('Renders correctly', () => {
-        const mockNavigation = { navigate: () => {} };
-        const tree = renderer.create(<CustomerSignIn navigation={mockNavigation} />);
-        expect(tree).toMatchSnapshot();
-    });
-
-    // it('Sign In button calls handleSignIn function', () => {
-    //    const { getByTestId } = render(<CustomerSignIn />);
-    //    spyOn(CustomerSignIn.prototype, 'handleSignIn')
-    //    fireEvent.press(getByTestId("SignInButton"));
-    //    expect(handleSignIn).toBeCalled();
-    // });
+  it('Renders correctly', () => {
+    const mockNavigation = { navigate: () => {} };
+    const tree = renderer.create(<CustomerSignIn navigation={mockNavigation} />);
+    expect(tree).toMatchSnapshot();
+  });
 });
